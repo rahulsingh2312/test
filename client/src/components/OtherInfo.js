f52acf2ea75f4cc930e5fb2aef2e80383aedb790
Ankit Chaudhary <ankitcha810@gmail.com>
2024-03-17 13:41:07 +0530

    client side done

---
+++ client/src/components/OtherInfo.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function OtherInfo({ formData, setFormData }) {
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFormData({
+        ...formData,
+        document: {
+          name: file.name,
+          file: file,
+        },
+      });
+    }
+  };
+
+  return (
+    <div className="other-info-container">
+      <input
+        type="text"
+        placeholder="Nationality..."
+        value={formData.nationality}
+        onChange={(e) => {
+          setFormData({ ...formData, nationality: e.target.value });
+        }}
+      />
+      <input
+        type="file"
+        accept=".pdf,.doc,.docx"
+        onChange={handleFileChange}
+      />
+    </div>
+  );
+}
+
+export default OtherInfo;
