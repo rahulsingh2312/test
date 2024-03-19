a9a4485c76d9f935d8c63fd5ec061ea75418fd5d
arvind2602 <guptaarvind2602@gmail.com>
2024-03-19 13:50:00 +0530

    Updated

--- client/src/hooks/useFormContext.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import FormContext from "@/context/FormContext";
-
-// using the context
-const useFormContext = () => {
-    return useContext(FormContext);
-};
-
-export default useFormContext;
